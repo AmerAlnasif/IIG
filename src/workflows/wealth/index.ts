0f38499857a21b5c0f31336654aa168a2cbf4ae9
AmerAlnasif <118650911+AmerAlnasif@users.noreply.github.com>
2026-08-23 23:39:03 +0300

    Create index.ts to export meeting note functions and types

---
+++ src/workflows/wealth/index.ts
@@ -0,0 +1,6 @@
+export {
+  intakeMeetingNote,
+  approveMeetingNote,
+  getApprovedMeetingNote,
+} from "./meeting-note-intake.ts";
+export type { MeetingNoteInput, MeetingNoteDraft } from "./meeting-note-intake.ts";
